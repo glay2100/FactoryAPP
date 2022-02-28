4ff64d54ff20a43ab8ff69deddb5a373ad2afaf8
glay2100 <yichen2100@gmail.com>
2022-03-01 01:34:37 +0800

    Dark&FloatingActionBtn

--- app/src/main/java/com/example/factoryapp/After_Login.kt
+++ app/src/main/java/com/example/factoryapp/After_Login.kt
@@ -1,36 +1,52 @@
 package com.example.factoryapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_after_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class After_Login : AppCompatActivity() {
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_login)
 
 
+        // 啟動activity後讀取登入activity(MainActivity)的帳號密碼資料
         intent?.extras?.let {
-            val value = it.getString("Key")
-            title = value
+
+            val accountOfBundle = it.getString("Account")
+            val passwordOfBundle = it.getString("Password")
+
+            // 將action bar title改成account
+            title = accountOfBundle
         }
 
 
+        // 建立RecyclerView的ArrayList
         val arrayList = ArrayList<Model>()
-
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-        arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-
         val myAdapter = MyAdapter(arrayList, this)
-
         recyclerView_of_AfterLogin.layoutManager = LinearLayoutManager(this)
         recyclerView_of_AfterLogin.adapter = myAdapter
+
+
+        // fabBtn為右下角"+"的button
+        val fabBtn = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        // 當fab被點擊時
+        fabBtn.setOnClickListener {
+
+            // 將RecyclerView新增一項
+            arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
+            // 更新列表
+            myAdapter.notifyDataSetChanged()
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/factoryapp/MainActivity.kt
+++ app/src/main/java/com/example/factoryapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.factoryapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val accountED = findViewById<EditText>(R.id.ed_account)
+        val passwordED = findViewById<EditText>(R.id.ed_password)
+
         // 經由button(login_taipei)啟動After_Login這個activity
         findViewById<Button>(R.id.btn_login_taipei).setOnClickListener {
             val intent = Intent(this, After_Login::class.java)
-            intent.putExtra("Key", "billy")
+
+            val accountPasswordBundle = Bundle()
+            accountPasswordBundle.putString("Account", accountED.text.toString())
+            accountPasswordBundle.putString("Password", passwordED.text.toString())
+
+            intent.putExtras(accountPasswordBundle)
             startActivity(intent)
         }
-        //
     }
 }
