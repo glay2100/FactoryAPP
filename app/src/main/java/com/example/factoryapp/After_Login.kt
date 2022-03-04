5a0f3c649aceea9cff4b6284f29fdb80759ed6fb
glay2100 <yichen2100@gmail.com>
2022-03-04 14:27:00 +0800

    Toast added

--- app/src/main/java/com/example/factoryapp/After_Login.kt
+++ app/src/main/java/com/example/factoryapp/After_Login.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_after_login.*
@@ -35,18 +36,39 @@ class After_Login : AppCompatActivity() {
         recyclerView_of_AfterLogin.adapter = myAdapter
 
 
+        // 建立在Toast選單中出現的項目的arraylist
+        val chart_ArrayItem = arrayOf("Chart 1", "Chart 2", "Chart 3", "Chart 4")
+
+
         // fabBtn為右下角"+"的button
         val fabBtn = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         // 當fab被點擊時
         fabBtn.setOnClickListener {
 
-            // 將RecyclerView新增一項
-            arrayList.add(Model("NewChart", "This is a Chart description", R.drawable.mpchart_pie))
-            // 更新列表
-            myAdapter.notifyDataSetChanged()
+            var position = 0
+
+            // 產生選單來選擇要insert哪一個chart
+            AlertDialog.Builder(this)
+                .setTitle("Selecting Chart")
+                .setSingleChoiceItems(chart_ArrayItem, 0) { dialogInterface, i ->
+                    position = i
+                }
+                .setPositiveButton("Insert") { dialog, which ->
+                    showToast("The Chart you insert is ${chart_ArrayItem[position]}")
+
+                    // 將RecyclerView新增一項
+                    arrayList.add(Model(chart_ArrayItem[position], "This is a Chart description", R.drawable.mpchart_pie))
+                    // 更新列表
+                    myAdapter.notifyDataSetChanged()
+                }.show()
+
+
+
         }
 
+    }
 
-
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
