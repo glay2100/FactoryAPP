9b63904368817e479006e90f3ceb967daa123755
glay2100 <yichen2100@gmail.com>
2022-03-24 23:57:39 +0800

    ItemTouchHelperAndDelete

--- app/src/main/java/com/example/factoryapp/After_Login.kt
+++ app/src/main/java/com/example/factoryapp/After_Login.kt
@@ -1,18 +1,28 @@
 package com.example.factoryapp
 
 import android.annotation.SuppressLint
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_after_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class After_Login : AppCompatActivity() {
     @SuppressLint("NotifyDataSetChanged")
+
+    private val arrayList = ArrayList<Model>()
+    private lateinit var myAdapter : MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_login)
@@ -30,10 +40,14 @@ class After_Login : AppCompatActivity() {
 
 
         // 建立RecyclerView的ArrayList
-        val arrayList = ArrayList<Model>()
-        val myAdapter = MyAdapter(arrayList, this)
+        myAdapter = MyAdapter(arrayList, this)
         recyclerView_of_AfterLogin.layoutManager = LinearLayoutManager(this)
         recyclerView_of_AfterLogin.adapter = myAdapter
+
+
+        // 將itemTouchHelper attach 到 recyclerView
+        val itemTouchHelper = ItemTouchHelper(simpleCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView_of_AfterLogin)
 
 
         // 建立在Toast選單中出現的項目的arraylist
@@ -42,9 +56,10 @@ class After_Login : AppCompatActivity() {
 
         // fabBtn為右下角"+"的button
         val fabBtn = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+
         // 當fab被點擊時
         fabBtn.setOnClickListener {
-
             var position = 0
 
             // 產生選單來選擇要insert哪一個chart
@@ -61,12 +76,39 @@ class After_Login : AppCompatActivity() {
                     // 更新列表
                     myAdapter.notifyDataSetChanged()
                 }.show()
-
-
-
         }
 
     }
+
+
+
+
+    val simpleCallback = object : ItemTouchHelper.SimpleCallback
+        (ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.START or ItemTouchHelper.END, 0) {
+        // 拖移 RecyclerView 裡的 items 會用到 onMove
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            val fromPosition = viewHolder.absoluteAdapterPosition
+            val toPosition = target.absoluteAdapterPosition
+            Collections.swap(arrayList, fromPosition, toPosition)
+
+            myAdapter.notifyItemMoved(fromPosition, toPosition)
+
+            return false
+        }
+
+        // 左右滑 RecyclerView 裡的 items 會用到 onSwiped
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+
+
 
     private fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/factoryapp/MyAdapter.kt
+++ app/src/main/java/com/example/factoryapp/MyAdapter.kt
@@ -1,10 +1,16 @@
 package com.example.factoryapp
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.row.view.*
 
 class MyAdapter(val arrayList: ArrayList<Model>, val context: Context) :
@@ -12,10 +18,13 @@ class MyAdapter(val arrayList: ArrayList<Model>, val context: Context) :
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val title_Tv = itemView.findViewById<TextView>(R.id.Title_Tv)
+        val description_Tv = itemView.findViewById<TextView>(R.id.description_Tv)
+        val image_Iv = itemView.findViewById<ImageView>(R.id.image_Iv)
         fun bindItems(model: Model) {
 
-            itemView.Title_Tv.text = model.title
-            itemView.description_Tv.text = model.des
+//            itemView.Title_Tv.text = model.title
+//            itemView.description_Tv.text = model.des
             itemView.image_Iv.setImageResource(model.image)
         }
     }
@@ -29,6 +38,19 @@ class MyAdapter(val arrayList: ArrayList<Model>, val context: Context) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(arrayList[position])
+        holder.title_Tv.text = arrayList[position].title
+        holder.description_Tv.text = arrayList[position].des
+
+
+        holder.image_Iv.setOnLongClickListener {
+            Snackbar.make(it, "刪除chart", Snackbar.LENGTH_LONG)
+                .setAction("確認") {
+                    arrayList.removeAt(position)
+                    notifyDataSetChanged()
+                }.show()
+
+            true
+        }
     }
 
     override fun getItemCount(): Int {
